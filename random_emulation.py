905bbc12c807a5b5cdcf52b58d6bf6b3ce740801
Omer-5 <OmerHazan5@gmail.com>
2024-03-14 23:13:34 +0200

    max table range

--- random_emulation.py
+++ random_emulation.py
@@ -1,14 +1,14 @@
 import time
 import paho.mqtt.client as mqtt
 import random
-from mqtt_init import *
+import mqtt_init
 
 class MqttEmulator:
-    def __init__(self):
+    def __init__(self, max_table_number):
         self.client = mqtt.Client()
-        self.client.username_pw_set(username, password)
+        self.client.username_pw_set(mqtt_init.username, mqtt_init.password)
         self.client.on_connect = self.on_connect
-        self.client.connect(broker_ip, int(broker_port))
+        self.client.connect(mqtt_init.broker_ip, int(mqtt_init.broker_port))
         self.client.loop_start()
 
     def on_connect(self, client, userdata, flags, rc):
@@ -16,7 +16,7 @@ class MqttEmulator:
             print("Connected to broker")
             self.start_emulation()
         else:
-            print("Failed to connect to broker")
+            print("Failed to connect to broker with error code:", rc)
 
     def start_emulation(self):
         while True:
@@ -28,45 +28,46 @@ class MqttEmulator:
             time.sleep(5)  # Adjust the interval as needed
 
     def send_temperature_message(self):
-        table_number = random.randint(1, 9999)
-        temperature = random.randint(10, 40)  # Random temperature between 10 to 40 degrees
+        table_number = random.randint(1, max_table_number)
+        temperature = random.randint(10, 40) 
         topic = "tables/tmp"
         message = f'{{"table_number": {table_number}, "temperature": {temperature}}}'
         print("Sending Temperature Message:", message)
         self.client.publish(topic, message)
 
     def send_light_message(self):
-        table_number = random.randint(1, 9999)
-        is_on = random.choice([True, False])  # Randomly choose between True or False
+        table_number = random.randint(1, max_table_number)
+        is_on = random.choice([True, False]) 
         topic = "tables/light"
         message = f'{{"table_number": {table_number}, "is_on": {str(is_on).lower()}}}'
         print("Sending Light Message:", message)
         self.client.publish(topic, message)
 
     def send_ac_message(self):
-        table_number = random.randint(1, 9999)
-        is_on = random.choice([True, False])  # Randomly choose between True or False
+        table_number = random.randint(1, max_table_number)
+        is_on = random.choice([True, False]) 
         topic = "tables/air_conditioner"
         message = f'{{"table_number": {table_number}, "is_on": {str(is_on).lower()}}}'
         print("Sending Air Conditioner Message:", message)
         self.client.publish(topic, message)
 
     def send_presence_message(self):
-        table_number = random.randint(1, 9999)
-        is_occupied = random.choice([True, False])  # Randomly choose between True or False
+        table_number = random.randint(1, max_table_number)
+        is_occupied = random.choice([True, False]) 
         topic = "tables/occupied"
         message = f'{{"table_number": {table_number}, "is_occupied": {str(is_occupied).lower()}}}'
         print("Sending Presence Message:", message)
         self.client.publish(topic, message)
 
     def send_call_waiter_message(self):
-        table_number = random.randint(1, 9999)
+        table_number = random.randint(1, max_table_number)
         topic = "tables/waiter_call"
         message = f'{{"table_number": {table_number}, "request": "call_waiter"}}'
         print("Sending Call Waiter Message:", message)
         self.client.publish(topic, message)
 
 if __name__ == "__main__":
-    mqtt_emulator = MqttEmulator()
+    max_table_number = 12 
+    mqtt_emulator = MqttEmulator(max_table_number)
     while True:
         time.sleep(1)  # Keep the main thread running
